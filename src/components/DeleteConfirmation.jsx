221eec414e17e989e8ae1f19bc21f3e93e16b228
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-12-17 11:36:30 +0700

    problem with object & function as dependencies (because they are not treated as equal, so it will cause an infinite loop)

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -9,7 +9,9 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
   return () => {
     clearTimeout(timer);
   }
-}, []); //This is a side effect because it's not directly related to outputting the jsx code. The jsx code will always be rendered and if we use condition ({open ? showcontent : null}), it's will cause a problem too. Because no matter the selection user chose (No or Yes), the dialog will close by itself after 3 seconds.
+}, [onConfirm]); //This is a side effect because it's not directly related to outputting the jsx code. The jsx code will always be rendered and if we use condition ({open ? showcontent : null}), it's will cause a problem too. Because no matter the selection user chose (No or Yes), the dialog will close by itself after 3 seconds.
+
+//Objects and functions are not treated as equal. So there may be a problem with objects and functions as dependencies (It will cause an infinite loop). Especially if there is a state update function in the function (set as dependency), because it will cause the App component to re-executed again and produce a new function, which we can't compare with the previous one (it always will be different).
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
